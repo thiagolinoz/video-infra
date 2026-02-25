f504850df6a5f300a903fe711279647c938e5f7a
thiagolinoz <thiagolinoz@gmail.com>
2026-02-25 16:49:26 -0300

    altera nome do bucket backend

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "postech-fiap-video-backend-eks"
+    bucket = "postech-fiap-video-backend-eks-dockerizado"
     key    = "backend/tfstate/terraform.tfstate"
     region = "us-east-1"
   }
