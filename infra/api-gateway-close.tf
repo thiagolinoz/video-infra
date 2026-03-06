f38545f92f35d3e19fa3a9fa2d6d33916c1c1815
maycon-alves <maycon_valdemiro@hotmail.com>
2026-03-06 00:09:25 -0300

    adicionando lambda authorized e gateway fechado

--- infra/api-gateway-close.tf
+++ infra/api-gateway-close.tf
@@ -8,7 +8,7 @@ resource "aws_api_gateway_authorizer" "lambda_authorizer" {
   rest_api_id             = aws_api_gateway_rest_api.app_video.id
   authorizer_uri         = "arn:aws:apigateway:us-east-1:lambda:path/2015-03-31/functions/${aws_lambda_function.authorizer.arn}/invocations" #aws_lambda_function.authorizer.invoke_arn
   #authorizer_credentials = var.role_lab
-  identity_source        = "method.request.header.Authorization"
+  #identity_source        = "method.request.header.Authorization"
   type                   = "REQUEST"
   #   provider_arns           = [aws_cognito_user_pool.main.arn]
   authorizer_result_ttl_in_seconds = 0 # <--- sem cache
@@ -28,39 +28,88 @@ resource "aws_api_gateway_resource" "app_video_v1" {
   path_part   = "v1"
 }
 
+# /api/v1/user
+resource "aws_api_gateway_resource" "app_video_user" {
+  rest_api_id = aws_api_gateway_rest_api.app_video.id
+  parent_id   = aws_api_gateway_resource.app_video_v1.id
+  path_part   = "user"
+}
 
-# resource "aws_api_gateway_deployment" "app_video_deployment" {
-#   depends_on = [
-# #     aws_api_gateway_integration.pessoa_item_integration,
-# #     aws_api_gateway_method.pessoa_item_get,
-# #     aws_api_gateway_authorizer.lambda_authorizer
-#   ]
-#
-#   rest_api_id = aws_api_gateway_rest_api.app_video.id
-#   description = "Deployment for /api/v1/pessoa/{cdDocPessoa}"
-#
-#   # Força nova deployment quando há mudanças
-#   triggers = {
-#     redeployment = sha1(jsonencode([
-# #       aws_api_gateway_resource.pessoa_item.id,
-# #       aws_api_gateway_method.pessoa_item_get.id,
-# #       aws_api_gateway_method.pessoa_post.id,
-# #       aws_api_gateway_integration.pessoa_item_integration.id,
-# #       aws_api_gateway_authorizer.lambda_authorizer.id,
-#     ]))
-#   }
-#
-#   lifecycle {
-#     create_before_destroy = true
-#   }
-# }
+# /api/v1/user/{email}
+resource "aws_api_gateway_resource" "app_video_email" {
+  rest_api_id = aws_api_gateway_rest_api.app_video.id
+  parent_id   = aws_api_gateway_resource.app_video_user.id
+  path_part   = "{email}"
+}
+
+###############################################
+# /api/v1/user/{email}/videos
+###############################################
+resource "aws_api_gateway_resource" "app_video_email_videos" {
+  rest_api_id = aws_api_gateway_rest_api.app_video.id
+  parent_id   = aws_api_gateway_resource.app_video_email.id
+  path_part   = "videos"
+}
+
+# GET /api/v1/user/{email}/videos
+resource "aws_api_gateway_method" "app_video_email_videos_GET" {
+  rest_api_id   = aws_api_gateway_rest_api.app_video.id
+  resource_id   = aws_api_gateway_resource.app_video_email_videos.id
+  http_method   = "GET"
+  authorization = "CUSTOM"
+  authorizer_id = aws_api_gateway_authorizer.lambda_authorizer.id
+
+  request_parameters = {
+    "method.request.header.Authorization" = true
+    "method.request.path.email"          = true
+  }
+}
+
+resource "aws_api_gateway_integration" "app_video_email_videos_GET_integration" {
+  rest_api_id             = aws_api_gateway_rest_api.app_video.id
+  resource_id             = aws_api_gateway_resource.app_video_email_videos.id
+  http_method             = aws_api_gateway_method.app_video_email_videos_GET.http_method
+  integration_http_method = "GET"
+  type                    = "HTTP_PROXY"
+  uri                     = "${var.host_video_app}/api/v1/user/{email}/videos"
+
+  request_parameters = {
+    "integration.request.header.Authorization" = "method.request.header.Authorization"
+    "integration.request.path.email"          = "method.request.path.email"
+  }
+}
+resource "aws_api_gateway_deployment" "app_video_deployment" {
+  depends_on = [
+     aws_api_gateway_integration.app_video_email_videos_GET_integration,
+#     aws_api_gateway_method.pessoa_item_get,
+#     aws_api_gateway_authorizer.lambda_authorizer
+  ]
+
+  rest_api_id = aws_api_gateway_rest_api.app_video.id
+  description = "Deployment for GET /api/v1/user/{email}/videos"
+
+  # Força nova deployment quando há mudanças
+  triggers = {
+    redeployment = sha1(jsonencode([
+       aws_api_gateway_resource.app_video_email_videos.id,
+       aws_api_gateway_method.app_video_email_videos_GET.id,
+#       aws_api_gateway_method.pessoa_post.id,
+       aws_api_gateway_integration.app_video_email_videos_GET_integration.id,
+#       aws_api_gateway_authorizer.lambda_authorizer.id,
+    ]))
+  }
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
 # Cria o stage 'prod'
-# resource "aws_api_gateway_stage" "video_app_stage" {
-#   stage_name    = "dev"
-#   rest_api_id   = aws_api_gateway_rest_api.app_video.id
-#   deployment_id = aws_api_gateway_deployment.app_video_deployment.id
-#
-#   variables = {
-#     # aqui você pode definir variáveis de stage se quiser
-#   }
-# }
+resource "aws_api_gateway_stage" "video_app_stage" {
+  stage_name    = "dev"
+  rest_api_id   = aws_api_gateway_rest_api.app_video.id
+  deployment_id = aws_api_gateway_deployment.app_video_deployment.id
+
+  variables = {
+    # aqui você pode definir variáveis de stage se quiser
+  }
+}
